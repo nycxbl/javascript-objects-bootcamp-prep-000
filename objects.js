5b1582265029658561dde1ca60ab2600455104f4
Benjamin Lee <nycxbl@hotmail.com>
2018-07-06 18:54:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 };
 
 function updatePlaylist(list, artist, title) {
-  Object.assign({}, list, {[artist]: title});
+  return Object.assign({}, list, {[artist]: title});
 }
 
 function removeFromPlaylist(list, artist) {
